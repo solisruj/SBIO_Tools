656e10f2b7b7e5c59c4502e3b2a35702900f8154
Jose Solis <solisruiz.j@gmail.com>
2019-01-28 18:56:34 -0800

    Adding some comments.

--- graphing/Boxplots.py
+++ graphing/Boxplots.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
-#!/usr/bin/python
-
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
+# Creating a simple TUI parser to take command line arguments. 
 def create_parser():
 	parser = argparse.ArgumentParser(description="This outputs a boxplot for a a given csv file containing one quantitative column and one catagorical column.")
 	parser.add_argument("-in", "--input_file", dest="input_file", type=str, nargs=1 , help="Input should be a two column csv file.", required=True)
@@ -20,12 +19,12 @@ def main():
 	input_file = myargs.input_file[0]
 	out_file = myargs.output_file[0]
 
+	# Importing data into a pandas dataframe and passing factors to a variable.
 	dataframe = pd.read_table(input_file, sep=",")
-
 	factors = dataframe.columns.tolist()
 
+	# Setting the boxplot from sns.catplot and saving the figure.
 	boxplot = sns.catplot(x=factors[1], y=factors[0], kind="box", data=dataframe)
-	
 	boxplot.savefig(out_file)
 
 if __name__ == '__main__':
