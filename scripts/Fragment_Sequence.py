cd532ba8aa769591d99d532a551ba14b66833176
Jose Solis <solisruiz.j@gmail.com>
2019-02-13 18:47:25 -0800

    Changed the interpreter path to call python 3, and made some basic syntax changes for Fragment_Sequence.py to run python 3.

--- scripts/Fragment_Sequence.py
+++ scripts/Fragment_Sequence.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 
 def concatenate(input):
 	seq = ''
-	for line in open(input, 'rb'):
+	for line in open(input, 'r'):
 		if (">" not in line):
 			seq += line.strip('\n')
 	return seq 
 
 def Formatting_to_Fasta(seq):
 	sequence_fasta_format = []
-	for n in xrange(0,len(seq), 70):
+	for n in range(0,len(seq), 70):
 		line = str(seq[n:n+70])+"\n"
 		sequence_fasta_format.append(line)
 	return sequence_fasta_format
 
 def fragment(seq):
 	fragments = []
-	for n in xrange(0,len(seq), 1000):
+	for n in range(0,len(seq), 1000):
 		line = str(seq[n:n+1000])+"\n"
 		fragments.append(line)
 	return fragments
