6cb0255a8d469c2da7e93f3d9b1ed3cf143d1785
Jose Solis <solisruiz.j@gmail.com>
2019-03-15 17:17:46 -0700

    Updated Trimmer.py by adding a command line interface and cleaning it up.

--- scripts/Trimmer.py
+++ scripts/Trimmer.py
@@ -1,27 +1,49 @@
+#!/usr/bin/env python
+
+
 import sys
+import argparse
 
-trim_start = sys.argv[1]
-trim_trailing = sys.argv[2]
-fasta_file = sys.argv[3]
-outfile = sys.argv[4]
+def create_parser():
+	parser = argparse.ArgumentParser(description="This program trims an aligned sequence.")
+	parser.add_argument("-ts", "--trim_start", dest="trim_start", type=str, nargs=1 , help="Trim n bases from the begining of the sequence.", required=True)
+	parser.add_argument("-te", "--trim_end", dest="trim_end", type=str, nargs=1 , help="Trim n bases from the end of the sequence.", required=True)
+	parser.add_argument("-in", "--input_fasta", dest="input_fasta", type=str, nargs=1 , help="Input alignment file.", required=True)
+	parser.add_argument("-out", "--output_file", dest="output_file", type=str, nargs=1, help="Name of the trimmed output file.", required=True) 
+	args = parser.parse_args()
+	return args
 
-contigs = {}
-for line in open(fasta_file, 'r'):
-	if (">" in line):
-		header = line
-		seq = ''
-	elif (">" not in line):
-		s = line.strip("\n")
-		seq += s
-	contigs[header] = seq
+def map_contigs(file):
+	contigs = {}
+	for line in open(file, 'r'):
+		if (">" in line):
+			header = line
+			sequence = ''
+		elif (">" not in line):
+			seq = line.strip("\n")
+			sequence += seq
+		contigs[header] = sequence
+	return contigs
 
-output = open(outfile, 'w')
+def main():
 
-for header in contigs.keys():
-	trim_end = len(contigs[header]) - int(trim_trailing)
-	output.write(header)
-	trimmed_seq = contigs[header][int(trim_start):int(trim_end)]
-	for n in xrange(0,len(trimmed_seq), 80):
-		output.write(str(trimmed_seq[n:n+80])+"\n")
+	myargs = create_parser()
+	trim_start = myargs.trim_start[0]
+	trim_trailing = myargs.trim_end[0]
+	fasta_file = myargs.input_fasta[0]
+	outfile = myargs.output_file[0]
 
-output.close()
+	contigs = map_contigs(fasta_file)
+
+	output = open(outfile, 'w')
+	for header in contigs.keys():
+		trim_end = len(contigs[header]) - int(trim_trailing)
+		output.write(header)
+		trimmed_seq = contigs[header][int(trim_start):int(trim_end)]
+		for n in xrange(0,len(trimmed_seq), 80):
+			output.write(str(trimmed_seq[n:n+80])+"\n")
+
+	output.close()
+
+if __name__ == '__main__':
+	main()
