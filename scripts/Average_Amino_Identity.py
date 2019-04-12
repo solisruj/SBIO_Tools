2a258e3ae98b629770ec6d2db53a6d8b9d033142
Jose Solis <solisruiz.j@gmail.com>
2019-04-12 10:31:43 -0700

    Updated AAI to use blastp instead of tblastn.

--- scripts/Average_Amino_Identity.py
+++ scripts/Average_Amino_Identity.py
@@ -20,40 +20,56 @@ def create_parser():
 	args = parser.parse_args()
 	return args
 
+def concatenate(input):
+	seq = ''
+	length = 0
+	for line in open(input, 'r'):
+		if (">" not in line):
+			length += len(line.strip('\n'))
+			seq += line.strip('\n')
+	return seq, float(length)
+
 def main():
 
 	myargs = create_parser()
 	query = myargs.query[0]
 	reference = myargs.reference[0]
 
-	make_blast_db = ["makeblastdb", "-in", reference, "-dbtype",  "nucl", "-out", "tmp.aai.reference.db"]
+	number_of_fragments = 0
+	for line in open(query, 'r'):
+		if ('>' in line):
+			number_of_fragments += 1
+
+	rseq, rlength = concatenate(reference)
+
+	make_blast_db = ["makeblastdb", "-in", reference, "-dbtype",  "prot", "-out", "tmp.aai.reference.db"]
 	subprocess.call(make_blast_db)
-	blast = ["tblastn",  "-query", query, "-db", "tmp.aai.reference.db", "-out", "tmp.aai.blast.out", "-outfmt", '6 qseqid qlen qstart qend sseqid slen sstart send length nident mismatch gaps score bitscore evalue pident']
+	blast = ["blastp",  "-query", query, "-db", "tmp.aai.reference.db", "-out", "tmp.aai.blast.out", "-outfmt", '6 qseqid qlen qstart qend sseqid slen sstart send length nident mismatch gaps score bitscore evalue pident']
 	subprocess.call(blast)
 
-	blast_dataframe = pd.read_table("tmp.aai.blast.out", sep="\t", header=None)
+	blast_dataframe = pd.read_csv("tmp.aai.blast.out", sep="\t", header=None)
 	blast_dataframe = blast_dataframe.drop_duplicates(subset=[blast_dataframe.columns[0]])
 
 	qseq_length = 0
-	number_of_fragments = 0
+	#bnumber_of_fragments = 0
 	sum_amino_percent_match = 0
-	rseq_length = 0
+	#rseq_length = 0
 	for i in range(len(blast_dataframe)):
 		#print blast_dataframe.iloc[i::+1, 8].values[0], blast_dataframe.iloc[i::+1, 9].values[0]
-		amino_percent_match = float(blast_dataframe.iloc[i::+1, 9].values[0]) // float(blast_dataframe.iloc[i::+1, 8].values[0])
+		amino_percent_match = float(blast_dataframe.iloc[i::+1, 9].values[0]) / float(blast_dataframe.iloc[i::+1, 8].values[0])
 		#print amino_percent_match, blast_dataframe.iloc[i::+1, 15]
-		if (amino_percent_match > 0.70 and blast_dataframe.iloc[i::+1, 15].values[0] > 0.30):
+		if (amino_percent_match > 0.70 and blast_dataframe.iloc[i::+1, 15].values[0] > 0.70):
 			sum_amino_percent_match += amino_percent_match
-			qseq_length += blast_dataframe.iloc[i::+1, 9].values[0]
-			rseq_length += blast_dataframe.iloc[i::+1, 8].values[0]
-			number_of_fragments += 1
+			qseq_length += blast_dataframe.iloc[i::+1, 8].values[0]
+			#rseq_length += blast_dataframe.iloc[i::+1, 8].values[0]
+			#bnumber_of_fragments += 1
 
-	score = ((sum_amino_percent_match // number_of_fragments ) ) * 100
-	coverage = (qseq_length // rseq_length ) * 100
-	similarity = coverage * score // 100
+	score = ((sum_amino_percent_match / number_of_fragments ) ) * 100
+	coverage = (qseq_length / rlength) * 100
+	similarity = coverage * score / 100
 
 	print
-	print sum_amino_percent_match, "\t", number_of_fragments, "\t", qseq_length, "\t", rseq_length, "\t", coverage, "\t", score, "\t", similarity
+	print sum_amino_percent_match, "\t", number_of_fragments, "\t", qseq_length, "\t", rlength, "\t", coverage, "\t", score, "\t", similarity
 	print
 
 	files_in_cwd = os.listdir(os.getcwd())
